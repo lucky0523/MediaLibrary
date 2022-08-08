9a5b8dce42ff78d43da5c6faee68b5c07c15bfca
lucky0523 <lucky0523@gmail.com>
2022-08-09 01:27:06 +0800

    页面只展示已匹配的电影；找不到的海外使用占位图代替

--- WebPage/views.py
+++ WebPage/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -10,20 +11,20 @@ from MediaLibrary.common import Static
 def imgs(request):
     # img_list = models.Img.objects.all()
     view_list = []
-    mlist = Media.objects.all()
+    mlist = Media.objects.filter(~Q(imdb_id=''))
     for media in mlist:
         view_dict = {}
         view_dict['year'] = media.release_date.year
-        view_dict['disk']=media.disk_sn
-        view_dict['path']=media.path
+        view_dict['disk'] = media.disk_sn
+        view_dict['path'] = media.path
+        view_dict['title'] = media.get_i18n_title()
         try:
-            view_dict['title'] = eval(media.i18n_title)[Static.LANGUAGE]
             view_dict['image'] = eval(media.image_paths)[Static.KEY_IMAGE_CATEGORY_POSTER][1:]
             print(view_dict['image'])
         except SyntaxError as e:
             print(e)
-            view_dict['title'] = '标题错误'
-            view_dict['image'] = ''
+        if 'image' not in view_dict or view_dict['image'] == '':
+            view_dict['image'] = '/static/images/no-image.png'
         print(view_dict)
         view_list.append(view_dict)
         pass
