f46710f5ddd0896cf63ae4b1777d24b3fd0b94b9
lucky0523 <lucky0523@gmail.com>
2022-08-13 00:20:41 +0800

    修改dialog样式；实现翻页功能10%

--- WebPage/views.py
+++ WebPage/views.py
@@ -10,8 +10,15 @@ from MediaLibrary.common import Static
 
 def imgs(request):
     # img_list = models.Img.objects.all()
+    one_page_count = 24
+    page = 1
+    try:
+        page = int(request.GET.get('page', 1))
+    except ValueError as e:
+        print(e)
     view_list = []
     mlist = Media.objects.filter(~Q(imdb_id=''))
+    mlist = mlist[one_page_count * (page - 1):one_page_count * page]
     for media in mlist:
         view_dict = {}
         view_dict['year'] = media.release_date.year
@@ -20,12 +27,10 @@ def imgs(request):
         view_dict['title'] = media.get_i18n_title()
         try:
             view_dict['image'] = eval(media.image_paths)[Static.KEY_IMAGE_CATEGORY_POSTER][1:]
-            print(view_dict['image'])
         except SyntaxError as e:
             print(e)
         if 'image' not in view_dict or view_dict['image'] == '':
             view_dict['image'] = '/static/images/no-image.png'
-        print(view_dict)
         view_list.append(view_dict)
         pass
     context = {}
